a991a6f29fe942b357f33695ca8b4bfd398f7946
test <test@test.com>
2014-12-10 01:11:14 +0100

    first commit

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-11-13 14:40:45 -0800"
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define middlefinger-qt message queue name
+#define BITCOINURI_QUEUE_NAME "middlefingerURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
